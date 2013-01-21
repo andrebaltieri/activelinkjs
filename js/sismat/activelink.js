26cf481c6e66861ce4767f1f1c3712e84935f49b
Andre Baltieri <andrebaltieri@hotmail.com>
2013-01-21 04:11:26 -0200

    Added JS file

---
+++ js/sismat/activelink.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    var curl = "";
+    var hurl = "";
+    curl += window.location;
+    hurl += window.location.host;
+
+    var surl = curl.replace("http://" + hurl, "");
+    $("a[href='" + surl + "']").addClass("activelinkjs-selected");
+});
